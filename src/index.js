c95aa33227e133ea81bb4d60282e3c29e47b5a8d
Krystian <krystian.czekalski@resourcekraft.com>
2022-09-19 17:26:12 +0100

    Update versions of React to v18, and react router to v6

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Auth from './components/auth'
 import * as serviceWorker from './serviceWorker';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { CookiesProvider } from 'react-cookie';
 
-function Router(){
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Auth />,
+    },
+    {
+        path: "/movies",
+        element: <App />,
+    },
+]);
 
-  return (
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
     <React.StrictMode>
-      <CookiesProvider>
-        <BrowserRouter>
-          <Route exact path="/" component={Auth} />
-          <Route exact path="/movies" component={App} />
-        </BrowserRouter>
-      </CookiesProvider>
+        <CookiesProvider>
+            <RouterProvider router={router} />
+        </CookiesProvider>
     </React.StrictMode>
-  )
-}
-
-ReactDOM.render(<Router />, document.getElementById('root'));
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
